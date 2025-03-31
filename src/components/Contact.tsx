8c24737df6d70c599963ca4e9a518d3a24682439
Ayushparwal <parwalayush5@gmail.com>
2025-03-31 15:48:26 +0530

    added_more_links

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -45,7 +45,8 @@ const Contact = () => {
       </div>
 
       <footer className='mt-10 text-sm opacity-80 text-center px-4'>
-        &copy; {new Date().getFullYear()} Made with ❤️ by Ayush Parwal. All rights reserved.
+        &copy; {new Date().getFullYear()} Made with ❤️ by Ayush Parwal. All rights reserved.<br/>
+        Contact: +91 7841072103
       </footer>
     </section>
   );
